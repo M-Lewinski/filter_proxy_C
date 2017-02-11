cb53450178c9eec6fc3d70f74ad0fc485dbfb728
Kamil <kpiotrowski@comea.pl>
2017-02-11 15:37:43 +0100

    Poprawk ido configu

--- source/config.c
+++ source/config.c
@@ -23,7 +23,7 @@ struct configStruct* loadConfigWithPath(char* filePath){
     config->headerRulesNumber = 0;
     config->block = (struct blockRule*)malloc(blockRuleMalloc*sizeof(struct blockRule));
     config->header = (struct headersRule*)malloc(headerRuleMalloc* sizeof(struct headersRule));
-    config->header = (struct headersRule*)malloc(cookieRuleMalloc* sizeof(struct headersRule));
+    config->cookie = (struct headersRule*)malloc(cookieRuleMalloc* sizeof(struct headersRule));
 
     while ((lineLen = getline(&line, &len, file)) != -1)
         if(parseLine(config, line, lineLen)!=0)
@@ -33,20 +33,32 @@ struct configStruct* loadConfigWithPath(char* filePath){
     fclose(file);
     return config;
 }
+
+struct headersRule newClearRule(){
+    struct headersRule r;
+    r.hostNamePattern=NULL;
+    r.hostNameRegex=NULL;
+    r.value=NULL;
+    r.namePattern=NULL;
+    r.nameRegex=NULL;
+    return r;
+}
 void freeHeaderStruct(struct headersRule ruleToFree){
-    regfree(ruleToFree.hostNameRegex);
-    regfree(ruleToFree.nameRegex);
+    if(ruleToFree.hostNameRegex!=NULL) regfree(ruleToFree.hostNameRegex);
+    if(ruleToFree.nameRegex!=NULL) regfree(ruleToFree.nameRegex);
     free(ruleToFree.namePattern);
     free(ruleToFree.hostNamePattern);
     free(ruleToFree.value);
 }
+void freeBlockStruct(struct blockRule ruleToFree){
+    free(ruleToFree.hostPattern);
+    if(ruleToFree.hostRegex!=NULL) regfree(ruleToFree.hostRegex);
+}
 
 void freeConfig(struct configStruct* config){
     int i;
-    for(i=0;i<config->blockRulesNumber;i++){
-        free(config->block[i].hostPattern);
-        regfree(config->block[i].hostRegex);
-    }
+    for(i=0;i<config->blockRulesNumber;i++)
+        freeBlockStruct(config->block[i]);
     for(i=0;i<config->cookieRulesNumber;i++)
         freeHeaderStruct(config->cookie[i]);
     for(i=0;i<config->headerRulesNumber;i++)
@@ -63,25 +75,23 @@ int parseLine(struct configStruct* config, char* line, ssize_t lineLen){
     while(line[0]=='\t' || line[0]==' ') line=line+1; //Removing whitespaces
     if(line[0]=='#' || line[0]=='\0') return 0; //Skipping empty lines and comments
 
-    int maxPartCount=10, partCount=1;
+    int maxPartCount=10, partCount=0;
     char** splittedLine = (char**)malloc(maxPartCount*sizeof(char*));
-    for(i=0;i<maxPartCount;i++) splittedLine[i]="\0";
+    for(i=0;i<maxPartCount;i++) splittedLine[i]="";
     char*lineCpy=(char*)malloc((strlen(line)+1)*sizeof(char));
-    strcpy(lineCpy, line);
-    splittedLine[0]=lineCpy;
-    char* splitPointer;
-    for(splitPointer = lineCpy+1;splitPointer[0]!='\0';splitPointer++) { //LINE SPLITTING
-        if(maxPartCount<partCount){
-            maxPartCount+=1;
+    lineCpy = strcpy(lineCpy,line);
+
+    char *tok = strtok(lineCpy," ");
+    while(tok!=NULL){
+        splittedLine[partCount]=tok;
+        partCount++;
+        tok = strtok(NULL," ");
+        if(maxPartCount<=partCount){
+            maxPartCount+=2;
             splittedLine=realloc(splittedLine,maxPartCount*sizeof(char*));
         }
-        if ((splitPointer[0] != ' ' || splitPointer[0] != '\t') &&
-            ((splitPointer - 1)[0] == ' ' || (splitPointer - 1)[0] == '\t')) {
-            splittedLine[partCount] = splitPointer;
-            (splitPointer - 1)[0] = '\0';
-            partCount++;
-        }
     }
+
     int toReturn;
     if(!strcmp(splittedLine[0],"BLOCK"))
         toReturn=parseBlockRule(config, splittedLine);
@@ -99,22 +109,22 @@ int parseLine(struct configStruct* config, char* line, ssize_t lineLen){
 int parseBlockRule(struct configStruct* config, char** splittedLine){
     char* blockPattern = splittedLine[1];
     if(strlen(blockPattern)<1)return -1;
-    struct blockRule* blockR = (struct blockRule*)malloc(1*sizeof(struct blockRule));
-    blockR->hostPattern=(char*)malloc((strlen(blockPattern)+1)*sizeof(char));
-    strcpy(blockR->hostPattern,blockPattern);
+    struct blockRule blockR;
 
-    regex_t *reg = (regex_t*)malloc(sizeof(regex_t));
-    if(regcomp(reg,blockR->hostPattern,0)){
-        fprintf(stderr,"Unable to compile regex: %s",blockR->hostPattern);
-        free(blockR);
+    blockR.hostPattern=(char*)malloc((strlen(blockPattern)+1)*sizeof(char));
+    blockR.hostRegex = (regex_t*)malloc(sizeof(regex_t));
+    strcpy(blockR.hostPattern,blockPattern);
+
+    if(regcomp(blockR.hostRegex,blockR.hostPattern,0)){
+        fprintf(stderr,"Unable to compile regex: %s",blockR.hostPattern);
+        freeBlockStruct(blockR);
         return -1;
     }
-    blockR->hostRegex=reg;
     if(config->blockRulesNumber>=blockRuleMalloc){
         blockRuleMalloc++;
         config->block=realloc(config->block,blockRuleMalloc*sizeof(struct blockRule));
     }
-    config->block[config->blockRulesNumber]=*blockR;
+    config->block[config->blockRulesNumber]=blockR;
     config->blockRulesNumber++;
     return 0;
 }
@@ -126,47 +136,43 @@ int parseHeaderRule(struct configStruct* config, char** splittedLine, int header
     else if(!strcmp(splittedLine[1],"CHA")) type = CHA;
     else return -1;
 
-    char* name=splittedLine[2];
+    struct headersRule rule = newClearRule();
+    rule.type=type;
+
+    char* name=splittedLine[2];     //Compiling header name regex
     if(strlen(name)<1) return -1;
-
-    char* value=NULL;
-    if(type!=DEL){
-        value=splittedLine[3];
-        if(strlen(value)<1) return -1;
-    }
-
-    char* host=NULL;
-    if(type!=DEL) host=splittedLine[4];
-    else host=splittedLine[3];
-
-    struct headersRule* rule = (struct headersRule*)malloc(1*sizeof(struct headersRule));
-    rule->type=type;
-
-    if(value!=NULL) rule->value = (char*)malloc((strlen(value)+1)*sizeof(char));
-    if(host!=NULL) rule->hostNamePattern = (char*)malloc((strlen(host)+1)*sizeof(char));
-    rule->namePattern = (char*)malloc((strlen(name)+1)*sizeof(char));
-
-    strcpy(rule->value,value);
-    strcpy(rule->hostNamePattern,host);
-    strcpy(rule->namePattern,name);
-
-    regex_t *nameReg=(regex_t*)malloc(sizeof(regex_t));
-    regex_t *hostReg=NULL;
-    if(regcomp(nameReg,rule->namePattern,0)){
-        free(rule);
+    rule.namePattern = (char*)malloc((strlen(name)+1)*sizeof(char));
+    strcpy(rule.namePattern,name);
+    rule.nameRegex=(regex_t*)malloc(sizeof(regex_t));
+    if(regcomp(rule.nameRegex,rule.namePattern,0)){
         fprintf(stderr,"Unable to compile regex: %s\n",name);
+        freeHeaderStruct(rule);
         return -1;
     }
-    if(host!=NULL) {
-        hostReg=(regex_t*)malloc(sizeof(regex_t));
-        if (regcomp(hostReg, rule->hostNamePattern, 0)) {
-            free(rule);
+
+    if(type!=DEL){                  //Value if type is not DELETE
+        char *value=splittedLine[3];
+        if(strlen(value)<1) {
+            freeHeaderStruct(rule);
+            return -1;
+        }
+        rule.value = (char*)malloc((strlen(value)+1)*sizeof(char));
+        strcpy(rule.value,value);
+    }
+
+    char* host;                     //Compiling host regex
+    if(type!=DEL) host=splittedLine[4];
+    else host=splittedLine[3];
+    if(strlen(host)>1) {
+        rule.hostNamePattern = (char*)malloc((strlen(host)+1)*sizeof(char));
+        strcpy(rule.hostNamePattern,host);
+        rule.hostNameRegex=(regex_t*)malloc(sizeof(regex_t));
+        if (regcomp(rule.hostNameRegex, rule.hostNamePattern, 0)) {
             fprintf(stderr, "Unable to compile regex: %s\n", host);
+            freeHeaderStruct(rule);
             return -1;
         }
     }
-    rule->hostNameRegex=hostReg;
-    rule->nameRegex=nameReg;
 
     switch (headerCookie){
         case 0:
@@ -174,7 +180,7 @@ int parseHeaderRule(struct configStruct* config, char** splittedLine, int header
                 headerRuleMalloc++;
                 config->header=realloc(config->header,headerRuleMalloc*sizeof(struct blockRule));
             }
-            config->header[config->headerRulesNumber]=*rule;
+            config->header[config->headerRulesNumber]=rule;
             config->headerRulesNumber++;
             break;
         case 1:
@@ -182,11 +188,10 @@ int parseHeaderRule(struct configStruct* config, char** splittedLine, int header
                 cookieRuleMalloc++;
                 config->cookie=realloc(config->cookie,cookieRuleMalloc*sizeof(struct blockRule));
             }
-            config->cookie[config->cookieRulesNumber]=*rule;
+            config->cookie[config->cookieRulesNumber]=rule;
             config->cookieRulesNumber++;
             break;
         default:
-            free(rule);
             exit(1);
     }
     return 0;
