477f3be1dfcbe10cc00ef1e587f2ae6830f7cbd4
Kamil <kamil.piotrowski.pol@gmail.com>
2017-02-20 20:04:42 +0100

    fltrowanie headerów response

--- source/proxy.c
+++ source/proxy.c
@@ -108,7 +108,7 @@ int handleServerResponse(struct requestStruct *reqStruct) {
     if(readData(reqStruct->serverResponse, reqStruct->serverSoc, reqStruct->time, reqStruct) < 0){
         return -1;
     }
-//    filterResponse(configStructure, reqStruct);
+    filterResponse(configStructure, reqStruct);
 
     int size;
     char* req = requestToString(*reqStruct->serverResponse,&size,1);
@@ -250,6 +250,7 @@ int sendRequest(struct requestStruct *request, int epoolFd) {
 //    printf("Utworzony socket: %d\n",request->serverSoc);
     int size;
     char* req = requestToString(*request->clientRequest,&size,0);
+    //printf("%s\n",req);
     if(sendAll(request->serverSoc,req,size) < 0){
         free(req);
         return -1;
